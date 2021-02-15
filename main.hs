e8012b7ae8213b81f789dd778a6650516d7822a9
Sam Uwe Alws <sam@samalws.com>
2021-02-15 00:24:09 +0000

    Update main.hs

--- main.hs
+++ main.hs
@@ -6,28 +6,6 @@ import Text.ParserCombinators.Parsec
 import Data.Either
 import System.Environment
 
-{-
-tryParse a b = do
-  pa <- parse termParser "term a" a
-  pb <- parse termParser "term b" b
-  return (pa, pb)
-
-tryParseAndRun a b = text0 where
-  text0 = either show text1 $ tryParse a b
-  text1 (pa, pb) = maybe "Failed to convert a to de bruijn" (text2 pb) $ toDeBruijn [] pa
-  text2 pb da = maybe "Failed to convert b to de bruijn" (text3 da) $ toDeBruijn [] pb
-  text3 da db = if (hasType [] da db) then "a :: b" else "a !:: b"
-
-mainRoutine = do
-  putStrLn "enter a:"
-  a <- getLine
-  putStrLn "enter b:"
-  b <- getLine
-  putStrLn $ tryParseAndRun a b
-
-main = mainRoutine >> main
--}
-
 main = do
   args <- getArgs
   m <- checkFile $ head args
